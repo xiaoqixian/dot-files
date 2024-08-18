1afff67874a875715cb972813e40b80da997aafb
lunar <lunar@mini.local>
2024-08-18 17:16:00 +0800

    brew install list

---
+++ brew_install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Date:   Sun Aug 18 17:10:05 2024
+# Mail:   lunar_ubuntu@qq.com
+# Author: https://github.com/xiaoqixian
+
+# This file tell brew to install some necessary packages
+brew install neovim, v2ray, rust, rust-language-server, go, gopls, lua-language-server, pyright, font-dejavu-sans-mono-nerd-font, eul, gsed
